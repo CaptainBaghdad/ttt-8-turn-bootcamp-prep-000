c7b38b66b1e6b2ab63e2228ffd1bfc8bae585aff
Jason Pitts <juniorjr083@gmail.com>
2018-05-20 22:27:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,26 +36,3 @@ def valid_move?(board, position)
     end
 end
 
-# check if position is taken
-def position_taken?(board, position)
-    if ((board[input_to_index(position)] == "X") || (board[input_to_index(position)] == "O"))
-        return true
-    else
-        return false
-    end
-end
-
-
-
-def turn(board)
-    puts "Please enter 1-9:"
-    input = gets.strip
-    if (valid_move?(board, input_to_index(input)))
-        move(board, input_to_index(input))
-        display_board(board)
-    else
-        if (!(valid_move?(board, input_to_index(input))))
-            turn(board)
-        end
-    end
-end
